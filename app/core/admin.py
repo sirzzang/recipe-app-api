ecd161bb2026e38104df5e9c411de8b56cd8c8e6
sirzzang <sirzzang@naver.com>
2021-03-29 17:24:16 +0900

    20210329 admin user page 추가

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 from core import models
 
@@ -8,6 +9,15 @@ class UserAdmin(BaseUserAdmin): # extend default django UserAdmin
     # list email, name and order by id
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', )}),
+        (
+            _('Permissions'), 
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important Dates'), {'fields': ('last_login', )})
+    )
 
 
 # register UserAdmin(admin page) to our custom User model
