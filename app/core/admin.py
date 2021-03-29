e2021d91c9fce8dbfb9b8d5c80a5f23e5ea0cc5f
sirzzang <sirzzang@naver.com>
2021-03-29 16:57:33 +0900

    20210329 user model admin 코드 추가

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# # Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin): # extend default django UserAdmin
+    # list email, name and order by id
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+# register UserAdmin(admin page) to our custom User model
+admin.site.register(models.User, UserAdmin) 
+
