272565d9808e1c8a27750ff7be9bf71ee99c01ea
sirzzang <sirzzang@naver.com>
2021-03-30 15:21:47 +0900

    20210330 db 연결 6번재까지  테스트 코드

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,7 +10,14 @@ class CommandTests(TestCase):
     def test_wait_for_db_ready(self):
         '''test wating for db when db is available'''
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            # what mock object does
             gi.return_value = True  # return the specified value
             call_command('wait_for_db')  # minitor how many times called
             self.assertEqual(gi.call_count, 1)  # `getitem` only called once?
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        '''test wating for db'''
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]  # 6번째에 return
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)  # `getitem` called 6 times?
